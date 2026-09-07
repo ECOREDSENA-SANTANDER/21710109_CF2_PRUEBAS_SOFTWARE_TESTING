0f46553eb76a7a17d1c76c094546c56e8a227187
lizmanchego <karinamanchego0416@gmail.com>
2026-09-07 17:56:20 -0500

    ADD Video YT + Podcast

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -22,7 +22,9 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/5sAgi467WMo?si=RpwCV1RRBavrHGm2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            figcaption Video. Implementación de pruebas y mejora del software
+
 
 </template>
 
